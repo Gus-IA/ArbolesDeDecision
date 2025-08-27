9ec99643ab80c36a12a38fa8dfe07afe56b8e333
Gus <garciagus1992@gmail.com>
2025-08-27 12:25:54 +0200

    add regresión y ejemplo overfitting

--- arboles_de_decision.py
+++ arboles_de_decision.py
@@ -6,6 +6,7 @@ from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt 
 import numpy as np
 from sklearn.datasets import make_moons
+from sklearn.tree import DecisionTreeRegressor
 
 # cargamos el modelo iris
 iris = load_iris()
@@ -99,4 +100,99 @@ plt.sca(axes[1])
 plot_decision_boundary(deep_tree_clf2, Xm, ym, axes=[-1.5, 2.4, -1, 1.5], iris=False)
 plt.title("min_samples_leaf = {}".format(deep_tree_clf2.min_samples_leaf), fontsize=14)
 plt.ylabel("")
+plt.show()
+
+
+
+# ---- Regresión ----
+
+# creamos unos datos aleatorios
+m = 200
+X = np.random.rand(m, 1)
+y = 4 * (X - 0.5) ** 2
+y = y + np.random.randn(m, 1) / 10
+
+
+
+# instanciamos dos modelos de árbol de regresión y los entrenamos
+tree_reg1 = DecisionTreeRegressor(random_state=42, max_depth=2)
+tree_reg2 = DecisionTreeRegressor(random_state=42, max_depth=3)
+tree_reg1.fit(X, y)
+tree_reg2.fit(X, y)
+
+# mostramos el gráfico
+def plot_regression_predictions(tree_reg, X, y, axes=[0, 1, -0.2, 1], ylabel="$y$"):
+    x1 = np.linspace(axes[0], axes[1], 500).reshape(-1, 1)
+    y_pred = tree_reg.predict(x1)
+    plt.axis(axes)
+    plt.xlabel("$x_1$", fontsize=18)
+    if ylabel:
+        plt.ylabel(ylabel, fontsize=18, rotation=0)
+    plt.plot(X, y, "b.")
+    plt.plot(x1, y_pred, "r.-", linewidth=2, label=r"$\hat{y}$")
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+plt.sca(axes[0])
+plot_regression_predictions(tree_reg1, X, y)
+for split, style in ((0.1973, "k-"), (0.0917, "k--"), (0.7718, "k--")):
+    plt.plot([split, split], [-0.2, 1], style, linewidth=2)
+plt.text(0.21, 0.65, "Depth=0", fontsize=15)
+plt.text(0.01, 0.2, "Depth=1", fontsize=13)
+plt.text(0.65, 0.8, "Depth=1", fontsize=13)
+plt.legend(loc="upper center", fontsize=18)
+plt.title("max_depth=2", fontsize=14)
+
+plt.sca(axes[1])
+plot_regression_predictions(tree_reg2, X, y, ylabel=None)
+for split, style in ((0.1973, "k-"), (0.0917, "k--"), (0.7718, "k--")):
+    plt.plot([split, split], [-0.2, 1], style, linewidth=2)
+for split in (0.0458, 0.1298, 0.2873, 0.9040):
+    plt.plot([split, split], [-0.2, 1], "k:", linewidth=1)
+plt.text(0.3, 0.5, "Depth=2", fontsize=13)
+plt.title("max_depth=3", fontsize=14)
+plt.show()
+
+
+# generamos un archivo a partir del .dot
+dot_data = export_graphviz(
+    tree_reg1,
+    out_file=None,
+    feature_names=iris.feature_names[3:],
+    class_names=iris.target_names,
+    rounded=True,
+    filled=True
+    )
+
+graph = Source(dot_data)
+graph.render("regresion_tree", format="pdf", view=False)
+
+
+# entrenamos dos modelos sin restricción 
+tree_reg1 = DecisionTreeRegressor(random_state=42) # overfitting
+tree_reg2 = DecisionTreeRegressor(random_state=42, min_samples_leaf=10)
+tree_reg1.fit(X, y)
+tree_reg2.fit(X, y)
+
+x1 = np.linspace(0, 1, 500).reshape(-1, 1)
+y_pred1 = tree_reg1.predict(x1)
+y_pred2 = tree_reg2.predict(x1)
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+
+# mostramos el gráfico
+plt.sca(axes[0])
+plt.plot(X, y, "b.")
+plt.plot(x1, y_pred1, "r.-", linewidth=2, label=r"$\hat{y}$")
+plt.axis([0, 1, -0.2, 1.1])
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", fontsize=18, rotation=0)
+plt.legend(loc="upper center", fontsize=18)
+plt.title("No restrictions", fontsize=14)
+
+plt.sca(axes[1])
+plt.plot(X, y, "b.")
+plt.plot(x1, y_pred2, "r.-", linewidth=2, label=r"$\hat{y}$")
+plt.axis([0, 1, -0.2, 1.1])
+plt.xlabel("$x_1$", fontsize=18)
+plt.title("min_samples_leaf={}".format(tree_reg2.min_samples_leaf), fontsize=14)
 plt.show()
